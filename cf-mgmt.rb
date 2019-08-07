bef3e4cd797130c5092ad5a2da95328fdf325ad8
Aaron Gershman <aegershman@gmail.com>
2019-08-06 23:55:19 -0500

    oops

--- cf-mgmt.rb
+++ cf-mgmt.rb
@@ -3,7 +3,7 @@ class CfMgmt < Formula
   homepage "https://github.com/pivotalservices/cf-mgmt"
   url "https://github.com/pivotalservices/cf-mgmt/releases/download/v1.0.28/cf-mgmt-osx"
   sha256 "9af3d31535f89b854523424a5c8aeb86752db655332490ccbf2edcd031392821"
-  version: "1.0.28"
+  version "1.0.28"
 
   def install
     mv "cf-mgmt-osx", "cf-mgmt"
