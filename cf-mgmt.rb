00a664bc257703f1d911080b13c1f8c4154769fa
Aaron Gershman <aegershman@gmail.com>
2019-08-06 23:54:31 -0500

    verison and homepage

--- cf-mgmt.rb
+++ cf-mgmt.rb
@@ -1,11 +1,9 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class CfMgmt < Formula
   desc "x"
-  homepage ""
+  homepage "https://github.com/pivotalservices/cf-mgmt"
   url "https://github.com/pivotalservices/cf-mgmt/releases/download/v1.0.28/cf-mgmt-osx"
   sha256 "9af3d31535f89b854523424a5c8aeb86752db655332490ccbf2edcd031392821"
+  version: "1.0.28"
 
   def install
     mv "cf-mgmt-osx", "cf-mgmt"
