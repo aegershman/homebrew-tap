fab2587edc1b3b9a6baac60efca0b9e874b2a30e
Aaron Gershman <aegershman@gmail.com>
2019-08-24 00:53:32 -0500

    kind update

--- kind.rb
+++ kind.rb
@@ -1,11 +1,8 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Kind < Formula
   desc "Kubernetes IN Docker - local clusters for testing Kubernetes"
   homepage "https://kind.sigs.k8s.io/"
-  url "https://github.com/kubernetes-sigs/kind/releases/download/v0.4.0/kind-darwin-amd64"
-  sha256 "023f1886207132dcfc62139a86f09488a79210732b00c9ec6431d6f6b7e9d2d3"
+  url "https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-darwin-amd64"
+  sha256 "b6a8fe2b3b53930a1afa4f91b033cdc24b0f6c628d993abaa9e40b57d261162a"
 
   def install
     mv "kind-darwin-amd64", "kind"
