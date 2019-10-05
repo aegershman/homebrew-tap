1fc3967debd15dcdd8504e00294d97275f980a02
aegershman <aegershman@gmail.com>
2019-10-05 15:54:37 -0500

    factor out params for kind.rb

--- kind.rb
+++ kind.rb
@@ -1,8 +1,10 @@
 class Kind < Formula
-  desc "Kubernetes IN Docker - local clusters for testing Kubernetes"
   homepage "https://kind.sigs.k8s.io/"
-  url "https://github.com/kubernetes-sigs/kind/releases/download/v0.5.1/kind-darwin-amd64"
-  sha256 "b6a8fe2b3b53930a1afa4f91b033cdc24b0f6c628d993abaa9e40b57d261162a"
+
+  v = "v0.0.1" # CI Managed
+  url "https://github.com/kubernetes-sigs/kind/releases/download/#{v}/kind-darwin-amd64"
+  version v
+  sha256 "b6a8fe2b3b53930a1afa4f91b033cdc24b0f6c628d993abaa9e40b57d261162a" # CI Managed
 
   def install
     mv "kind-darwin-amd64", "kind"
