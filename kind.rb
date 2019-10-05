20411b091f4b3c0b6413c5f158f7cd72cfb323a1
aegershman <aegershman@gmail.com>
2019-10-05 17:19:35 -0500

    update sha

--- kind.rb
+++ kind.rb
@@ -4,7 +4,7 @@ class Kind < Formula
   v = "v0.0.1" # CI Managed
   url "https://github.com/kubernetes-sigs/kind/releases/download/#{v}/kind-darwin-amd64"
   version v
-  sha256 "b6a8fe2b3b53930a1afa4f91b033cdc24b0f6c628d993abaa9e40b57d261162a" # CI Managed
+  sha256 "b6a8fe2b3b53930a1afa4f91b033cdc24b0f6c628d993abaa9e40b57d261162a"
 
   def install
     mv "kind-darwin-amd64", "kind"
