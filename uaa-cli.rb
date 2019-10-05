4af6bd1424ec1fbf807195493effef128e7edf2d
aegershman <aegershman@gmail.com>
2019-10-05 18:10:30 -0500

    uaa downgrade real quick

--- uaa-cli.rb
+++ uaa-cli.rb
@@ -1,10 +1,10 @@
 class UaaCli < Formula
   homepage "https://github.com/cloudfoundry-incubator/uaa-cli"
 
-  v = "0.8.0"
+  v = "0.7.0"
   url "https://github.com/cloudfoundry-incubator/uaa-cli/releases/download/#{v}/uaa-darwin-amd64-#{v}"
   version v
-  sha256 "d63bf174f37ae4af375a287c97127c7d8c0d3fb4fae5dd6e5337b2cb524a3796"
+  sha256 "2139b4eb70d0f56193cbd97e5e6dc5c83b208d37423882a70a7e9b3081528792"
 
   def install
     mv "uaa-darwin-amd64*", "uaa"
