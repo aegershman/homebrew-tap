1d6d9858401e9bb95b6870050d36527e52e04608
aegershman <aegershman@gmail.com>
2019-10-05 17:25:06 -0500

    replaces version

--- kind.rb
+++ kind.rb
@@ -1,7 +1,7 @@
 class Kind < Formula
   homepage "https://kind.sigs.k8s.io/"
 
-  v = "v0.0.1" # CI Managed
+  v = "v0.5.1"
   url "https://github.com/kubernetes-sigs/kind/releases/download/#{v}/kind-darwin-amd64"
   version v
   sha256 "b6a8fe2b3b53930a1afa4f91b033cdc24b0f6c628d993abaa9e40b57d261162a"
