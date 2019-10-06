84dfc2a9bf2b57d0f8f7598fa6fa80f0b71f79e0
aegershman <aegershman@gmail.com>
2019-10-06 12:52:37 -0500

    variant bump

--- variant.rb
+++ variant.rb
@@ -2,11 +2,11 @@ class Variant < Formula
   homepage "https://github.com/mumoshu/variant"
   desc "Wrap up your bash scripts into a modern CLI today. Graduate to a full-blown golang app tomorrow."
 
-  v = "v0.35.0"
+  v = "v0.35.1"
   @@verNum = v.sub "v", ""
   url "https://github.com/mumoshu/variant/releases/download/#{v}/variant_#{@@verNum}_darwin_amd64.tar.gz"
   version v
-  sha256 "4d4c48e95859498055eb5f1be8f3c95491a52639e0298342937081f39b39bdae"
+  sha256 "5412fc84397dcc50c5a4dbc4f7362c02f8fa05755b0950aa4471fb9f2ed5b657"
 
   def install
     bin.install "variant"
