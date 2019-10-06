2db490c6fce51064dbec6dcba95035410e23a352
aegershman <aegershman@gmail.com>
2019-10-05 21:46:50 -0500

    shell for updating... which kind of defeats taskfile. But it's a WIP

--- main.sh
+++ main.sh
@@ -2,19 +2,19 @@
 
 case "$1" in
 uaa-cli)
-  GH_ORG=cloudfoundry-incubator
-  GH_REPO=uaa-cli
-  VERSION=0.8.0
-  ARTIFACT=uaa-darwin-amd64-0.8.0
-  TAP_RUBY_FILE=uaa-cli.rb
+  export GH_ORG=cloudfoundry-incubator
+  export GH_REPO=uaa-cli
+  export VERSION=0.8.0
+  export ARTIFACT=uaa-darwin-amd64-0.8.0
+  export TAP_RUBY_FILE=uaa-cli.rb
   ;;
 
 kind)
-  GH_ORG=kubernetes-sigs
-  GH_REPO=kind
-  VERSION=v0.5.1
-  ARTIFACT=kind-darwin-amd64
-  TAP_RUBY_FILE=kind.rb
+  export GH_ORG=kubernetes-sigs
+  export GH_REPO=kind
+  export VERSION=v0.5.1
+  export ARTIFACT=kind-darwin-amd64
+  export TAP_RUBY_FILE=kind.rb
   ;;
 esac
 
