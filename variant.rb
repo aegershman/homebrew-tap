a75ed7e65bb78b92a46a04e01a55d1da2b64575f
aegershman <aegershman@gmail.com>
2019-10-06 12:42:31 -0500

    testing variant once more

--- variant.rb
+++ variant.rb
@@ -6,7 +6,7 @@ class Variant < Formula
   @@verNum = v.sub "v", ""
   url "https://github.com/mumoshu/variant/releases/download/#{v}/variant_#{@@verNum}_darwin_amd64.tar.gz"
   version v
-  sha256 "5412fc84397dcc50c5a4dbc4f7362c02f8fa05755b0950aa4471fb9f2ed5b657"
+  sha256 "4d4c48e95859498055eb5f1be8f3c95491a52639e0298342937081f39b39bdae"
 
   def install
     bin.install "variant"
