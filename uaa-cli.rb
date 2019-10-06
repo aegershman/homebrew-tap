d1fb450dd50db1bb14cc7819961835477271b760
aegershman <aegershman@gmail.com>
2019-10-05 23:59:55 -0500

    autocommit

--- uaa-cli.rb
+++ uaa-cli.rb
@@ -1,7 +1,7 @@
 class UaaCli < Formula
   homepage "https://github.com/cloudfoundry-incubator/uaa-cli"
 
-  v = ""
+  v = "0.8.0"
   url "https://github.com/cloudfoundry-incubator/uaa-cli/releases/download/#{v}/uaa-darwin-amd64-#{v}"
   version v
   sha256 "d63bf174f37ae4af375a287c97127c7d8c0d3fb4fae5dd6e5337b2cb524a3796"
