f0607e42eaa5cbe9ee11ee6afdc0dc268a18d8cb
Aaron Gershman <aegershman@users.noreply.github.com>
2019-10-06 00:35:55 -0500

    separate versions from version-files as staging area (#6)
    * versions and sha separate

--- main.sh
+++ main.sh
@@ -5,6 +5,7 @@ uaa-cli)
   export GH_ORG=cloudfoundry-incubator
   export GH_REPO=uaa-cli
   export VERSION=0.8.0
+  export VERSION_FOLDER=uaa-cli
   export ARTIFACT=uaa-darwin-amd64-0.8.0
   export TAP_RUBY_FILE=uaa-cli.rb
   ;;
@@ -13,6 +14,7 @@ kind)
   export GH_ORG=kubernetes-sigs
   export GH_REPO=kind
   export VERSION=v0.5.1
+  export VERSION_FOLDER=kind
   export ARTIFACT=kind-darwin-amd64
   export TAP_RUBY_FILE=kind.rb
   ;;
@@ -21,6 +23,7 @@ fetch)
   export GH_ORG=gruntwork-io
   export GH_REPO=fetch
   export VERSION=v0.3.7
+  export VERSION_FOLDER=fetch
   export ARTIFACT=fetch_darwin_amd64
   export TAP_RUBY_FILE=fetch.rb
   ;;
