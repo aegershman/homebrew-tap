11952517b2e35307db95dd15c16bb7cd92d0194d
Aaron Gershman <aegershman@gmail.com>
2019-11-18 16:08:04 -0600

    clusterawsadm cluster api to 0.4.5

--- cluster-api-provider-aws.rb
+++ cluster-api-provider-aws.rb
@@ -2,10 +2,10 @@ class ClusterApiProviderAws < Formula
   desc "Kubernetes-native declarative infrastructure for AWS."
   homepage "https://github.com/kubernetes-sigs/cluster-api-provider-aws"
 
-  v = "v0.4.2"
+  v = "v0.4.5"
   url "https://github.com/kubernetes-sigs/cluster-api-provider-aws/releases/download/#{v}/clusterawsadm-darwin-amd64"
   version v
-  sha256 "cb5d517cb8a70f0f183a43cdb3f6322263042e6d3737344e965b4ea49b1ea553"
+  sha256 "9534f14106de8e05ecd800cc775ea38f4c97e4a34a4acef65db5ccc970456885"
 
   def install
     mv Dir.glob("clusterawsadm-darwin-amd64*").first, "clusterawsadm"
