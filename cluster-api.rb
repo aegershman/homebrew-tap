6136e91c552fbf56ff6fe4d6ecaa9ad1feb5dd35
Aaron Gershman <aegershman@gmail.com>
2019-11-18 16:07:18 -0600

    cluster-api clusterctl to 0.2.7

--- cluster-api.rb
+++ cluster-api.rb
@@ -2,10 +2,10 @@ class ClusterApi < Formula
   desc "Home for the Cluster Management API work, a subproject of sig-cluster-lifecycle"
   homepage "https://cluster-api.sigs.k8s.io"
 
-  v = "v0.2.5"
+  v = "v0.2.7"
   url "https://github.com/kubernetes-sigs/cluster-api/releases/download/#{v}/clusterctl-darwin-amd64"
   version v
-  sha256 "9e2cf24bdd25e78fdbfb2bb2a85fe24864eb63f63485b23991b24af2b1579433"
+  sha256 "0fa4c23386262614d21bb3dbf15a9798f5880d70d87ca7c2ec4ccb6ced1736b7"
 
   def install
     mv Dir.glob("clusterctl-darwin-*").first, "clusterctl"
